05e0de46d4c4786ddd69a96309add99639f7be93
linalol1488 <postnikovshkaya@mail.ru>
2025-06-05 12:07:24 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,28 +1,60 @@
-a = [4, 5, 6, 7, 8, 1]
-print(a[-1])
+from turtle import *
 
-print(a[-1])
+shape("turtle")
+penup()
+colormode(255)
+bgcolor("pink")
+pencolor("red")
+speed(200)
+forward(100)
+penup()
+backward(50)
+left(90)
+pendown()
+for i in range(4):
+    forward(20)
+    left(90)
+penup()
+backward(40)
+pendown()
 for i in range(3):
-    print(i)
-print(a)
+    forward(20)
+    left(120)
+penup()
+backward(40)
+pendown()
+for i in range(5):
+    forward(20)
+    left(36)
+    forward(20)
+    right(108)
+penup()
+backward(40)
+pendown()
+for i in range(3):
+    forward(20)
+    left(120)
+right(90)
+for i in range(4):
+    forward(20)
+    left(90)
+penup()
+forward(40)
+pendown()
+circle(40)
 
-count1 = 0
-count2 = 0
-for i in range(len(a)):
-    if a[i] % 2 == 0:
-        print(a[i])
-        count2 += 1
-    if a[i] % 2 == 1:
-        count1 += a[i]
-print(a)
-print(count1)
-print(count2)
+
+for i in range(360):
+    pencolor(i % 256, 0, i % 256)
+    forward(30)
+    right(90)
+    fd(1)
+    right(90)
+    fd(30)
+    left(90)
+    fd(1)
+    left(90)
 
 
-s = [5, 4, 5, 3, 2, 5, 5, 4, 5, 4, 3, 5,]
-j = 0
-for i in range(len(s)):
-    j = j + s[i]
-j = j/len(s)
-print(j)
 
+done()
